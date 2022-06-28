2e806b9f24fc117bfb106f6b06f54f7446133c4c
Jacob Kearns <jacob.kearns@playtech.com>
2022-06-28 15:34:56 +0100

    FirstDraft

---
+++ public/Robyn/main.js
@@ -0,0 +1,17 @@
+const btn = document.getElementById('hello-btn')
+
+btn.addEventListener('click', () => {
+  //window.alert('Welcome to my webiste')
+  document.body.style.backgroundColor = "black";
+})
+
+
+btn.addEventListener('dblclick', () => {
+  //window.alert('Welcome to my webiste')
+  document.body.style.backgroundColor = "brown";
+})
+
+
+
+
+
